d504102d84ae5f215ad96c223998e01ec7b5f51d
mtcattin <cat16002@byui.edu>
2017-06-02 18:56:46 -0600

    filterOverlay content div added 1.0

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,21 @@
 
-var filterOverlayElement = document.getElementsByClassName('hidden');
+/*var filterOverlayElement = document.getElementsByClassName('hidden');
 document.addEvent('click', function(event) {
   var isClickable = filterOverlayElement.contains(event.target);
   
   if (!isClickable) {
     document.getElementsByClassName('hidden').style.display="none";
   }
-});
+});*/
+
 /*Start of example  https://www.w3schools.com/howto/howto_js_sidenav.asp*/
 function openFilterOverlay() {
-  document.getElementById("mySidenav").style.width = "80%";
+  document.getElementById("myFilterOverlay").style.width = "80%";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function exitFilterNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("myFilterOverlay").style.width = "0";
   document.body.style.backgroundColor = "white";
 }
 /*End of example  https://www.w3schools.com/howto/howto_js_sidenav.asp */
