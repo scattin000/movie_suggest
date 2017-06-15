39c13203c554834b079e8996708a5ab15beb5606
mtcattin <cat16002@byui.edu>
2017-06-15 16:40:18 -0600

    don't think there are any changes...

--- scripts/main.js
+++ scripts/main.js
@@ -7,20 +7,28 @@ const BAD = 400;
 var apiKey = "1788420828765e6eb1a60d9453e534d1"; // api Key 
 var Req; // variable that will store requests
 
-/*var filterOverlayElement = document.getElementsByClassName('hidden');
+/*var touchOut = document.getElementsByClassName('filterOverlay');
 document.addEvent('click', function(event) {
   var isClickable = filterOverlayElement.contains(event.target);
   
   if (!isClickable) {
-    document.getElementsByClassName('hidden').style.display="none";
+    document.getElementsByClassName('filterOverlay').style.display="none";
   }
 });*/
 
 /*Start of example  https://www.w3schools.com/howto/howto_js_sidenav.asp*/
 function openFilterOverlay() {
     document.getElementById("myFilterOverlay").style.width = "80%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
 }
+//var touchOut = document.getElementsByClassName('filterOverlay');
+//document.addEvent('click', function(event) {
+//  var isClickable = filterOverlayElement.contains(event.target);
+//  
+//  if (!isClickable) {
+//    document.getElementsByClassName('filterOverlay').style.display="none";
+//  }
+//});
 
 function exitFilterNav() {
     document.getElementById("myFilterOverlay").style.width = "0";
