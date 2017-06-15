7a812dcef41e8743670d9ac302a3f5cb53ed8bb0
Sarah Cattin <scattin000@hotmail.com>
2017-06-15 17:29:42 -0600

    trying to make it all work again..

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,363 @@
 /* VARIABLES */
-// Set up request status code 
-const NOT_FOUND = 404;
-const OK = 200;
-const BAD = 400;
 
-var apiKey = "1788420828765e6eb1a60d9453e534d1"; // api Key 
-var Req; // variable that will store requests
 
-/*var touchOut = document.getElementsByClassName('filterOverlay');
+
+/*var filterOverlayElement = document.getElementsByClassName('hidden');
+
 document.addEvent('click', function(event) {
+
   var isClickable = filterOverlayElement.contains(event.target);
+
   
+
   if (!isClickable) {
-    document.getElementsByClassName('filterOverlay').style.display="none";
+
+    document.getElementsByClassName('hidden').style.display="none";
+
   }
+
 });*/
 
+
+
 /*Start of example  https://www.w3schools.com/howto/howto_js_sidenav.asp*/
+
 function openFilterOverlay() {
+
     document.getElementById("myFilterOverlay").style.width = "80%";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.9)";
+
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+
 }
-//var touchOut = document.getElementsByClassName('filterOverlay');
-//document.addEvent('click', function(event) {
-//  var isClickable = filterOverlayElement.contains(event.target);
-//  
-//  if (!isClickable) {
-//    document.getElementsByClassName('filterOverlay').style.display="none";
-//  }
-//});
+
+
 
 function exitFilterNav() {
+
     document.getElementById("myFilterOverlay").style.width = "0";
+
     document.body.style.backgroundColor = "white";
+
 }
+
 /*End of example  https://www.w3schools.com/howto/howto_js_sidenav.asp */
 
-/** Request top rated movies*/
-function request_TopRatedMovies() {
-    //URL: /discover/movie?sort_by=popularity.desc
 
-}
-/** Display only 3 top movies */
 
-/** Display details of the selected movie */
-function displayDetails() {
+// Variables for API Key
 
-}
+var apiKey = "api_key=1788420828765e6eb1a60d9453e534d1"; // api Key 
+
+//var Req; // variable that will store requests
+
+var imageBase = 'http://image.tmdb.org/t/p/';
+
+var apiBase = 'http://api.themoviedb.org/3';
+
+
+
+// Function for calling the Most Popular Movie API 
+
+function MostPopular() {
+
+    //want this funtion to go on load as well 
+
+    baseUrl_Image();
+
+
+
+    var urlPopular = apiBase + '/movie/popular?' + apiKey;
+
+    //setting up the new request
+
+    var xhr = new XMLHttpRequest();
+
+
+
+    document.getElementById("result").innerHTML = 'Most Popular';
+
+    document.getElementById("moviePosters").innerHTML = '';
+
+    // making the request
+
+    // figure out how to make these less than PG13 ratings only!!
+
+    xhr.open('GET', encodeURI(urlPopular));
+
+    xhr.onload = function() {
+
+        if (xhr.status === 200) {
+
+            // read the data in the JSON 
+
+            var mydata = JSON.parse(xhr.responseText);
+
+        } else {
+
+            alert('Request failed.  Returned status of ' + xhr.status);
+
+        }
+
+
+
+        console.log(mydata);
+
+        baseurl = window.movies_image_url;
+
+        console.log(baseurl);
+
+        posterSizes = window.movies_posters_sizes;
+
+        // only display top 3 movies
+
+        for (var i = 0; i < 3; i++) {
+
+            // variables for the details... need to store these to a different array?? OR need to make a seperate call?
+
+            /*var movies_title = mydata.results[i].original_title;
+
+            var movies_overview = mydata.results[i].overview;
+
+            var movies_votes = mydata.results[i].vote_average;
+
+            var movies_rating = mydata.results[i].genre_ids*/
+
+
+
+            var posters = mydata.results[i].poster_path;
+
+            // creating the image to display  & everything to hold it
+
+            var posters_Url = baseurl + posterSizes + posters;
+
+            console.log(posters_Url);
+
+            // get the HTML element
+
+            var div = document.getElementById('moviePosters');
+
+            // set up HTML for images 
+
+            var imgDiv = document.createElement('div')
+
+            imgDiv.className = "col-md-7 movie_poster"; // class name
+
+            var img = document.createElement('img');
+
+            // actually setting the image 
+
+            img.setAttribute('src', posters_Url);
+
+            //attach the image to the imgDiv section
+
+            imgDiv.appendChild(img);
+
+
+
+            // store details about the movie in this array 
+
+            //imageDescription is a property of the imgDiv object 
+
+            imgDiv.imageDescription = mydata.results[i];
+
+            // this function is within the img (selecting) adding method to an instance
+
+            imgDiv.onclick = function() {
+
+
+
+                // First Create the overlay page to display the informaion
+
+                var detailDisplay = document.createElement('section');
+
+                //create an id
+
+                detailDisplay.className = "movieDetails";
+
+
+
+                // set the title 
+
+                var titleDisplay = document.createTextNode(this.imageDescription.original_title);
+
+                var overviewDisplay = document.createTextNode(this.imageDescription.overview);
+
+
+
+                // append what we're working with to the detail display
+
+                this.appendChild(detailDisplay);
+
+                // append the details of the movie to the section
+
+                detailDisplay.appendChild(titleDisplay);
+
+                detailDisplay.appendChild(document.createElement('br'));
+
+                detailDisplay.appendChild(overviewDisplay);
+
+                // test the item to display 
+
+                //detailDisplay.innerHTML = "Hello";
+
+                /*
+
+                var node = document.createElement("section");
+
+                node.className = "divNode"; // set up the class
+
+                // These are the details "descriptions" - creating for HTML
+
+                var textnode = document.createTextNode(this.imageDescription.original_title); //movies_title);
+
+                var textOverview = document.createTextNode(this.imageDescription.movies_overview);
+
+                var textVote = document.createTextNode(this.imageDescription.movies_votes);
+
+                var textRatting = document.createTextNode('Ratings ');
+
+
+
+                // add these items within the "div" node 
+
+                node.appendChild(textnode);
+
+                //headingforMovies.appendChild(textnode);
+
+                node.appendChild(document.createElement('br'));
+
+                node.appendChild(document.createElement('br'));
+
+                node.appendChild(textRatting)
+
+                node.appendChild(textVote);
+
+                //node.appendChild(document.createElement('br'));
+
+                //node.appendChild(document.createElement('br'));
+
+                node.appendChild(textOverview);
+
+
+
+                //console.log(mydata.results[i].original_title);
+
+                document.getElementById("moviePosters").appendChild(node);
+
+                document.getElementById("moviePosters").appendChild(detailDisplay);*/
+
+            }
+
+
+
+            /*
+
+                        WANT THIS TO BE IN THE DETAILS OVERLAY 
+
+                        var node = document.createElement("div");
+
+                        node.className = "col-md-5 movie_desc "; // set up the class
+
+                        // These are the details "descriptions" - creating for HTML
+
+                        var textVote = document.createTextNode(movies_votes);
+
+                        var textRatting = document.createTextNode('Ratings ');
+
+
+
+                        // add these items within the "div" node
+
+                        node.appendChild(textRatting)
+
+                        node.appendChild(textVote);
+
+                        //node.appendChild(document.createElement('br'));
+
+                        //node.appendChild(document.createElement('br'));
+
+                        node.appendChild(textOverview);
+
+
+
+                        //console.log(mydata.results[i].original_title);
+
+                         document.getElementById("moviePosters").appendChild(node);
+
+*/
+
+            document.getElementById("moviePosters").appendChild(imgDiv);
+
+        }
+
+        //document.getElementById("result").innerHTML = mydata;
+
+    };
+
+    xhr.send();
+
+};
+
+
+
+// Config function for Movie Poster API. 
+
+function baseUrl_Image() {
+
+    var xhr = new XMLHttpRequest();
+
+    var data = [];
+
+    document.getElementById("result").innerHTML = 'BaseURL';
+
+    document.getElementById("moviePosters").innerHTML = '';
+
+
+
+    xhr.open('GET', encodeURI('http://api.themoviedb.org/3/configuration?api_key=12865ae0cb6774f79f4732530a443938'));
+
+    xhr.onload = function() {
+
+        if (xhr.status === 200) {
+
+            //alert('User\'s name is ' + xhr.responseText);
+
+            data.push(xhr.responseText);
+
+            var mydata = JSON.parse(data);
+
+        } else {
+
+            alert('Request failed.  Returned status of ' + xhr.status);
+
+        }
+
+
+
+        movies_image_url = mydata.images.base_url;
+
+        console.log(movies_image_url);
+
+        movies_posters_sizes = mydata.images.poster_sizes[2];
+
+        console.log(movies_posters_sizes);
+
+    };
+
+    xhr.send();
+
+    //return movies_image_url;
+
+};
+
+/** display the sub page  
+
+function setMovieDetails() {
+
+    var movies_title = mydata.results[i].original_title;
+
+    var movies_overview = mydata.results[i].overview;
+
+    var movies_votes = mydata.results[i].vote_average;
+
+}*/
